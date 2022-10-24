af03b1b8a380fdcce0af8c54c6c11ae3a0df7cc0
syahrulawaludin <syahrulawaludin72@gmail.com>
2022-10-24 14:55:43 +0700

    switch

---
+++ src/SwitchDenganYield.java
@@ -0,0 +1,20 @@
+public class SwitchDenganYield {
+
+    public static void main(String[] args) {
+        
+        // var nilai = "A";
+        // String  ucapan = switch (nilai) {
+        //     // case "A":
+        //     // yield "Wow anda lulus dengan baik";
+        //     // case "B" :
+        //     // yield "Lulus dengan baik";
+        //     // case "C" :
+        //     // yield "Anda tidak lulus";
+        //     // default :
+        //     // yield "Mungkin anda salah jurusan";
+            
+        // }
+        // System.out.println(ucapan);
+    }
+    
+}
