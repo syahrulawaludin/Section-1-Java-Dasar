69aafb52d9e2208921377c19aabf3a23cf5fc27b
syahrulawaludin <syahrulawaludin72@gmail.com>
2022-10-24 15:22:01 +0700

    perulangan

---
+++ src/ForLoop.java
@@ -0,0 +1,29 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        
+
+        // For Loop
+
+        // For adalah salah kata kunci yang bisa digunakan untuk melakukan perulangan 
+        // Blok kode yang  terdapat di dalam for akan selalu diulangi selama kondisi for terpenuhi
+
+
+        // var counter = 1;
+        //  for (; counter <= 10;){
+        //     System.out.println("Perulangan" + counter);
+        //     counter++;
+        //  }
+
+        //  System.out.println(counter);
+
+         
+         for (var counter2 = 1; counter2 <= 30; counter2++){
+            System.out.println("Perulangan" + counter2);
+            
+         }
+        
+
+
+    }
+    
+}
