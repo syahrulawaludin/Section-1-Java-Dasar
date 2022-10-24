02f227efbbf84e8701add0d25a548c771917c631
syahrulawaludin <syahrulawaludin72@gmail.com>
2022-10-24 16:38:20 +0700

    method

---
+++ src/Method.java
@@ -0,0 +1,22 @@
+public class Method {
+
+    public static void main(String[] args) {
+        
+        // Method adalah block kode program yang akan berjalan saat kita panggil
+        // Sebelumnya kita sudah menggunakan method println() untuk menampilkan tulisan di console
+        // Untuk membuat method di Java, kita bisa menggunakan kata kunci void, lalu diikuti dengan nama method, kurung () dan diakhiri dengan block
+        // Kita bisa memanggil method dengan menggunakan nama method lalu diikuti dengan kurunng ()
+        // Di bahasa pemrograman lain, Method juga disebut dengan function
+         
+        sayHelloWorld();
+        sayHelloWorld();
+        sayHelloWorld();
+    }
+    static void sayHelloWorld() {
+        System.out.println("Hello World");
+        System.out.println("Hello World 2");
+        System.out.println("Hello World 3");
+    }
+
+    
+}
