72fbe189dba67212d9a1d62bbf7c9a4e8746fd8d
syahrulawaludin <syahrulawaludin72@gmail.com>
2022-10-24 16:28:52 +0700

    for each

---
+++ src/Countinue.java
@@ -0,0 +1,15 @@
+public class Countinue {
+
+    public static void main(String[] args) {
+        
+        for (int counter = 1; counter <= 100; counter++) {
+            if (counter % 2 == 0) {
+                continue;
+            }
+
+
+            System.out.println("Perulangan Ganjil-" + counter);
+        }
+    }
+    
+}
