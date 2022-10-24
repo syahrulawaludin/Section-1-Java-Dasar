a37d7c68b02dc3271e588ae3776bdb1b1d2369b1
syahrulawaludin <syahrulawaludin72@gmail.com>
2022-10-24 14:03:52 +0700

    udpate block

---
+++ src/Expression.java
@@ -0,0 +1,16 @@
+public class Expression {
+    public static void main(String[] args) {
+        
+
+        
+        //  Expression adalah konstruksi dari variabel, operator, dan pemanggilan method yang mengevalueasi menadi sebuah singel value
+        // Expression adlaah core component dari statement
+
+        
+        int value;
+        value = 10;
+
+        System.out.println(value = 100);
+    }
+    
+}
---
+++ src/Statement.java
@@ -0,0 +1,27 @@
+public class Statement {
+    public static void main(String[] args) {
+        
+
+        // Statement
+        // Statement bisa dibilang adlaah kalimat lengka dalam bahasa
+        // Sebuah statement berisikan excution komplit, biasanya diakhhiri dengan titik koma
+        //  ada beberapa jenis statement
+
+
+        // Assigment statement
+        double aValue  = 8933.234;
+        // Increment statement
+
+        aValue++;
+
+        // Method invpcation statement
+        System.out.println("Hello World");
+
+        // Objek creation statement
+        Date datae = new Date();
+
+        // Block
+        // Block adalah kumpulan statement yang terdiri dari nol atau lebih statement
+        //  block diawali dan diakhiri dengan kurung kurawal {}
+    }
+}
