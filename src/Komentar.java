7c8dc474a1a98d38ac37a9dc763347d3cc61fe50
syahrulawaludin <syahrulawaludin72@gmail.com>
2022-10-25 09:24:06 +0700

    final

---
+++ src/Komentar.java
@@ -0,0 +1,15 @@
+public class Komentar {
+
+    public static void main(String[] args) {
+        
+        // Komentar
+
+        // Singeline
+
+        /**
+         * 
+         * 
+         */
+    }
+    
+}
