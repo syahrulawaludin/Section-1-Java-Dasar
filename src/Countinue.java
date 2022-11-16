ae42fd663bd69ed86094aa511b08ec534de944ad
syahrulawaludin <syahrulawaludin72@gmail.com>
2022-11-16 12:02:24 +0700

    update block java

--- src/Countinue.java
+++ src/Countinue.java
@@ -2,7 +2,7 @@ public class Countinue {
 
     public static void main(String[] args) {
         
-        for (int counter = 1; counter <= 100; counter++) {
+        for (int counter = 1; counter <= 20; counter++) {
             if (counter % 2 == 0) {
                 continue;
             }
